febc0863096eaf494995b8d18bebe636f83972fb
rabiaaydmr <48756062+rabiaaydmr@users.noreply.github.com>
2020-01-20 15:12:52 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace server
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_port = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button_listen = new System.Windows.Forms.Button();
+            this.logs = new System.Windows.Forms.RichTextBox();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.textBox_message = new System.Windows.Forms.TextBox();
+            this.button_send = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox_port
+            // 
+            this.textBox_port.Location = new System.Drawing.Point(175, 132);
+            this.textBox_port.Name = "textBox_port";
+            this.textBox_port.Size = new System.Drawing.Size(272, 31);
+            this.textBox_port.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(79, 135);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Port:";
+            // 
+            // button_listen
+            // 
+            this.button_listen.Location = new System.Drawing.Point(468, 126);
+            this.button_listen.Name = "button_listen";
+            this.button_listen.Size = new System.Drawing.Size(111, 43);
+            this.button_listen.TabIndex = 2;
+            this.button_listen.Text = "Listen";
+            this.button_listen.UseVisualStyleBackColor = true;
+            this.button_listen.Click += new System.EventHandler(this.button_listen_Click);
+            // 
+            // logs
+            // 
+            this.logs.Location = new System.Drawing.Point(84, 216);
+            this.logs.Name = "logs";
+            this.logs.Size = new System.Drawing.Size(673, 326);
+            this.logs.TabIndex = 3;
+            this.logs.Text = "";
+            // 
+            // Label2
+            // 
+            this.Label2.AutoSize = true;
+            this.Label2.Location = new System.Drawing.Point(79, 600);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(106, 25);
+            this.Label2.TabIndex = 4;
+            this.Label2.Text = "Message:";
+            // 
+            // textBox_message
+            // 
+            this.textBox_message.Enabled = false;
+            this.textBox_message.Location = new System.Drawing.Point(192, 600);
+            this.textBox_message.Name = "textBox_message";
+            this.textBox_message.Size = new System.Drawing.Size(428, 31);
+            this.textBox_message.TabIndex = 5;
+            // 
+            // button_send
+            // 
+            this.button_send.Enabled = false;
+            this.button_send.Location = new System.Drawing.Point(639, 589);
+            this.button_send.Name = "button_send";
+            this.button_send.Size = new System.Drawing.Size(118, 53);
+            this.button_send.TabIndex = 6;
+            this.button_send.Text = "send";
+            this.button_send.UseVisualStyleBackColor = true;
+            this.button_send.Click += new System.EventHandler(this.button_send_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(906, 735);
+            this.Controls.Add(this.button_send);
+            this.Controls.Add(this.textBox_message);
+            this.Controls.Add(this.Label2);
+            this.Controls.Add(this.logs);
+            this.Controls.Add(this.button_listen);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_port);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_port;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button_listen;
+        private System.Windows.Forms.RichTextBox logs;
+        private System.Windows.Forms.Label Label2;
+        private System.Windows.Forms.TextBox textBox_message;
+        private System.Windows.Forms.Button button_send;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace server
+{
+    public partial class Form1 : Form
+    {
+
+        Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        List<Socket> clientSockets = new List<Socket>();
+
+        bool terminating = false;
+        bool listening = false;
+
+        String[] names = new String[300];
+        List<String> names_include = new List<string>();
+
+        public Form1()
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;
+            this.FormClosing += new FormClosingEventHandler(Form1_FormClosing);
+            InitializeComponent();
+
+
+            //  Reading People Name to Check
+            int counter = 0;
+            string line;
+
+            System.IO.StreamReader file = new System.IO.StreamReader("user_db.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                names[counter] = line;
+                counter++;
+            }
+            file.Close();
+        }
+
+        private void button_listen_Click(object sender, EventArgs e)
+        {
+            int serverPort;
+
+            if (Int32.TryParse(textBox_port.Text, out serverPort))
+            {
+                IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, serverPort);
+                serverSocket.Bind(endPoint);
+
+                serverSocket.Listen(300);       //300 since we are going to listen 300 items.
+
+                listening = true;
+                button_listen.Enabled = false;
+                textBox_message.Enabled = true;
+                button_send.Enabled = true;
+
+                Thread acceptThread = new Thread(Accept);
+                acceptThread.Start();
+
+                logs.AppendText("Started listening on port: " + serverPort + "\n");
+
+            }
+            else
+            {
+                logs.AppendText("Please check port number \n");
+            }
+        }
+
+        private void Accept()
+        {
+            while (listening)
+            {
+                try
+                {
+                    Socket newClient = serverSocket.Accept();
+
+                    Byte[] our_name = new Byte[64];     //Names length is 64 Bytes.
+                    newClient.Receive(our_name);        //Name is received.
+                    string new_name = Encoding.Default.GetString(our_name);
+                    new_name = new_name.Substring(0, new_name.IndexOf("\0"));
+
+
+                    if (names.Contains(new_name) == true && names_include.Contains(new_name) != true)    //Checking if the name is in the database or not.
+                    {
+                        names_include.Add(new_name);
+
+                        clientSockets.Add(newClient);       //Adding the client to socket.
+                        logs.AppendText("Client : " + new_name + " is connected.");
+                        logs.AppendText(" \n");
+                        Thread our_thread = new Thread(Receive);
+                        our_thread.Start();
+                        string special_approval = "approved";
+                        Byte[] our_buffer = Encoding.Default.GetBytes(special_approval);
+                        newClient.Send(our_buffer);
+                    }
+                    else    //Name is not in the database.
+                    {
+
+                        string special_approval = "not_approved";
+                        if (names_include.Contains(new_name) == true)
+                        {
+                            special_approval = "same_name";
+                        }
+                        Byte[] our_buffer = Encoding.Default.GetBytes(special_approval);
+                        newClient.Send(our_buffer);
+                        newClient.Close();
+                        logs.AppendText("A client tried to connect with invalid name.\n");
+                    }
+                }
+                catch
+                {
+                    if (terminating)
+                    {
+                        listening = false;
+                    }
+                    else
+                    {
+                        logs.AppendText("The socket stopped working.\n");
+                    }
+
+                }
+            }
+        }
+
+        private void Receive()
+        {
+            Socket thisClient = clientSockets[clientSockets.Count() - 1];
+            bool connected = true;
+
+            string name_about_to = "";
+
+            while (connected && !terminating)
+            {
+                try
+                {
+                    Byte[] buffer = new Byte[64];
+                    thisClient.Receive(buffer);
+
+                    string incomingMessage = Encoding.Default.GetString(buffer);
+                    incomingMessage = incomingMessage.Substring(0, incomingMessage.IndexOf("\0"));
+
+                    int index_of = incomingMessage.IndexOf(":");
+                    name_about_to = incomingMessage.Substring(0, index_of);
+
+                    clientSockets.Remove(thisClient);       //We are removing this client so, he/she does not send message to himslef/herself.
+                    if (clientSockets.Count() > 0)      //If we have more than 1 client we can send a message.
+                    {
+                        foreach (Socket client in clientSockets)      //We are sending to message to each client one by one except this sender.
+                        {
+                            try
+                            {
+                                logs.AppendText("Client: " + incomingMessage + "\n");
+                                client.Send(buffer);
+                            }
+                            catch
+                            {
+                                logs.AppendText("Problem Occured with the connection. \n");
+                                terminating = true;
+                                textBox_message.Enabled = false;
+                                button_send.Enabled = false;
+                                textBox_port.Enabled = true;
+                                button_listen.Enabled = true;
+                                serverSocket.Close();
+
+                            }
+
+                        }
+
+                    }
+                    else
+                    {
+                        logs.AppendText("There is only one client to send. To send this message add more clients. \n");
+                    }
+                    clientSockets.Add(thisClient);
+                }
+                catch
+                {
+                    if (!terminating)
+                    {
+                        logs.AppendText("A client has disconnected\n");
+                    }
+
+                    if (name_about_to != "")
+                    {
+                        if (names_include.Contains(name_about_to) == true)
+                        {
+                            names_include.Remove(name_about_to);
+                        }
+                    }
+
+                    thisClient.Close();
+                    clientSockets.Remove(thisClient);
+                    connected = false;
+                }
+            }
+        }
+
+        private void Form1_FormClosing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            listening = false;
+            terminating = true;
+            Environment.Exit(0);
+        }
+
+        private void button_send_Click(object sender, EventArgs e)
+        {
+            string message = "Server: "+textBox_message.Text;
+            if (message != "" && message.Length <= 64)
+            {
+                Byte[] buffer = Encoding.Default.GetBytes(message);
+
+                foreach (Socket client in clientSockets)
+                {
+                    try
+                    {
+                        client.Send(buffer);
+                    }
+                    catch
+                    {
+                        logs.AppendText("There is a problem! Check the connection...\n");
+                        terminating = true;
+                        textBox_message.Enabled = false;
+                        button_send.Enabled = false;
+                        textBox_port.Enabled = true;
+                        button_listen.Enabled = true;
+                        serverSocket.Close();
+                    }
+
+                }
+            }
+        }
+    }
+}
